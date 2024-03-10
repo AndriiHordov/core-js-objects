471b84b4e7619b3be6c7ce2684882df3eee6fa7a
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-03-10 15:07:43 +0100

    feat: implement the mergeObjects function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,16 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+  objects.forEach((object) => {
+    const keys = Object.entries(object);
+    for (let i = 0; i < keys.length; i += 1) {
+      const [key, value] = keys[i];
+      result[key] = result[key] ? result[key] + value : value;
+    }
+  });
+  return result;
 }
 
 /**
